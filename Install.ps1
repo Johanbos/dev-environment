c63dbba362eca3eb2659fc91e549f2c213c72e77
Johan Bos <johan.bos@viaselect.com>
2017-02-08 14:50:02 +0100

    awesomeness in a script

---
+++ Install.ps1
@@ -0,0 +1,47 @@
+# run lines seperate!
+
+# Install a Windows 10 Home of Prof
+
+# Split drive to get a C with 100GB+ and D with 40GB+ (computer Management > Disk management)
+
+# Add to domain (ask domain admin)
+
+# Install all updates!!!
+
+# packagemanager
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+choco upgrade lessmsi -y
+
+# Google drive
+choco upgrade googledrive
+
+# passwords
+choco upgrade keepass -y
+choco upgrade keepass-keepasshttp -y
+
+# source control
+choco upgrade git -y
+choco upgrade sourcetree -y
+
+# Browsers
+choco upgrade googlechrome -y
+choco upgrade firefox -y
+
+# coding
+choco upgrade visualstudiocode -y
+choco upgrade visualstudio2015community -y
+choco upgrade docker-for-windows -y
+
+# dba
+choco upgrade -y
+
+# tools
+choco upgrade greenshot -y
+choco upgrade slack -y
+choco upgrade slack -y
+choco upgrade treesizefree -y
+choco upgrade procexp -y
+choco upgrade 7zip -y
+
+# update everything! :-)
+choco upgrade all -y
--- README.md
+++ README.md
@@ -1,9 +1,2 @@
 # powershell it as you go!
-
-# Install a Windows 10 Home of Prof
-# Split drive to get a C with 100GB+ and D with 40GB+ (computer Management > Disk management)
-
-# Add to domain (ask domain admin
-
-# Install packagemanager
-iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+Use powershell to create a customized develop environment
