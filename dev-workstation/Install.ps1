5ce53e392b5eef2040ebce9db328a49957fe92f2
Johan Bos <Johanbos@users.noreply.github.com>
2025-07-05 06:36:46 +0200

    Fix script exit

--- dev-workstation/Install.ps1
+++ dev-workstation/Install.ps1
@@ -5,7 +5,7 @@ write-host "Powershell version: " $PSVersionTable.PSVersion
 If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {   
     $arguments = "& '" + $myinvocation.mycommand.definition + "'"
     Start-Process powershell -Verb runAs -ArgumentList $arguments
-    Break
+    exit
 }
 
 # allow our own powershell scripts
